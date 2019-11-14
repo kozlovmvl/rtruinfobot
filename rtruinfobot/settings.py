e961fc8c15550bd0227cf93da675171ae0361d08
kozlovmv <kozlov.mvl@yandex.ru>
2019-11-14 10:17:08 +0300

    Изменен settings.py.

--- rtruinfobot/settings.py
+++ rtruinfobot/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pxajc&bpq)w!=k2$ck3chf!x&_y$o(^177)_h^0ck*(%a0j(f+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
